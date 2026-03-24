6328a974d138a7aa3b7237605ad6d6f707dd8c27
xyrevex <ralsei.the.fluffy.prince.of.dark@gmail.com>
2026-03-24 19:22:05 +1100

    Update upload.js

--- api/upload.js
+++ api/upload.js
@@ -21,7 +21,7 @@ export const config = {
 
 export default async function handler(req, res) {
   res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
   if (req.method === "OPTIONS") {
