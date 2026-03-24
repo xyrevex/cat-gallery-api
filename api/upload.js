1b6b5ce9dfdb34ba2a545751c1115e31a68b6250
xyrevex <ralsei.the.fluffy.prince.of.dark@gmail.com>
2026-03-24 17:54:46 +1100

    Update upload.js

--- api/upload.js
+++ api/upload.js
@@ -9,6 +9,12 @@ const r2 = new AWS.S3({
 });
 
 export default async function handler(req, res) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+  if (req.method === "OPTIONS") return res.status(200).end();
+
   try {
     const filename = req.query.filename;
     const type = req.query.type;
