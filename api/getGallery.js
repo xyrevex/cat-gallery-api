9fd01da96ad47b0fbfef3768d42049e3a58cd46a
xyrevex <ralsei.the.fluffy.prince.of.dark@gmail.com>
2026-03-24 19:25:39 +1100

    Update getGallery.js

--- api/getGallery.js
+++ api/getGallery.js
@@ -16,7 +16,7 @@ export default async function handler(req, res) {
   res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
-  if (req.method === "OPTIONS") {
+  if (req.method === "GET") {
     return res.status(200).end(); 
   }
 
