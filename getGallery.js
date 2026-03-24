8981736287cbdb5fbc38491861265d23afd745c1
xyrevex <ralsei.the.fluffy.prince.of.dark@gmail.com>
2026-03-24 17:19:04 +1100

    Add files via upload

---
+++ getGallery.js
@@ -0,0 +1,15 @@
+export default async function handler(req, res) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+
+  const API_KEY = process.env.JSONBIN_KEY;
+  const BIN_ID = process.env.BIN_ID;
+
+  const response = await fetch(`https://api.jsonbin.io/v3/b/${BIN_ID}/latest`, {
+    headers: {
+      "X-Master-Key": API_KEY
+    }
+  });
+
+  const data = await response.json();
+  res.status(200).json(data.record);
+}
